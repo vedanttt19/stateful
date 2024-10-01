f5599709e88008bc5794548be1c6294c3c16b65e
vedanttt19 <vedantkendre489@gmail.com>
2024-10-01 19:24:17 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,61 +1,53 @@
+
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const PlayersApp());
+void main(){
+  runApp (const AppbarColorApp());
 }
-
-class PlayersApp extends StatefulWidget {
-  const PlayersApp({super.key});
-
- @override
-   State<PlayersApp> createState() => _PlayersAppState();
+class AppbarColorApp extends StatefulWidget{
+  const AppbarColorApp({super.key});
+  @override
+  State<AppbarColorApp> createState()=>
+   _AppbarColorAppState();
 }
-
-class _PlayersAppState extends State<PlayersApp> {
- int _counter = 0;
-
-List<String> playerList = <String>[
-  "https://cdn.britannica.com/25/222725-050-170F622A/Indian-cricketer-Mahendra-Singh-Dhoni-2011.jpg",
-   "https://www.hindustantimes.com/static-content/1y/cricket-logos/players/virat-kohli.png",
-    "https://imgeng.jagran.com/images/2023/apr/Sachin11682274529876.jpg",
-      "https://bsmedia.business-standard.com/_media/bs/img/article/2022-03/15/full/1647365891-7103.jpg",
-        "https://images.indianexpress.com/2023/09/Rahul-15.jpg",
- ];
-
- @override
- Widget build(BuildContext context) {
-  return MaterialApp(
-   title: 'Players',
-    home: Scaffold(
-     appBar: AppBar(
-      backgroundColor: Colors.blue,
-       title: const Text("Players App"),
-       centerTitle: true,
-       ),
-        body: Center(
-         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-           Image.network(
-           playerList[_counter],
-           height: 300,
-         ),
-        ],
-       ),
-     ),
-    floatingActionButton: FloatingActionButton(
-     onPressed: () {
-    if (_counter < playerList.length - 1) {
-    _counter++;
-    } else {
-    _counter = 0;
-    }
-    setState(() {});
-   },
-    tooltip: 'Increment',
-      child: const Icon(Icons.add),
-    ),
-    ),
-  );
+class _AppbarColorAppState extends
+ State<AppbarColorApp>{
+  bool colorChange= true;
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Appbar Color",
+      home:Scaffold(
+        appBar: AppBar(
+          title:const Text ("Appbar Color App"),
+          centerTitle: true,
+          backgroundColor: (colorChange) ? 
+          Colors.amber: Colors.blue,
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: 
+            MainAxisAlignment.center,
+            children:[
+              Text("Change Appbar Color"),
+            ],
+          ),
+        ),
+        floatingActionButton:
+        FloatingActionButton(
+          onPressed:(){
+            if (colorChange){
+              colorChange= false;
+            } else {
+              colorChange= true;
+            }
+            setState((){});
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
  }
-}
